2262b20df82263457e604a242fc26e271869e49e
Kushagra Goel <kushagra2632002goel@gmail.com>
2020-07-20 23:15:38 +0530

    twitter api

---
+++ Course 3/Week 6/hidden.py
@@ -0,0 +1,5 @@
+def oauth():
+    return {"consumer_key": "B8aFvbw4O2QYxOTkptRd1nm4I",
+            "consumer_secret": "vEyssuKZ6Ps1aurESWLDs70LHIOzupvWNQE0H36xb4Ylkwp14m",
+            "token_key": "1065527798113214464-mwFkkNTHuRErw9qO7oaR4LUOGwvFjO",
+            "token_secret": "L7tB2qSU4qqyhiHOZL5GH3HgD297jfwW9H0U4HrgIyT9p"}
