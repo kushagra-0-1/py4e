7b57d845003f05bc88503118f80667c90045f29f
Kushagra Goel <kushagra2632002goel@gmail.com>
2020-07-30 22:55:52 +0530

    first_class

--- Course 4/Week 1/first_class.py
+++ Course 4/Week 1/first_class.py
@@ -1,2 +1,12 @@
-class first_class:
+class PartyAnimal:
     x=0
+    
+    def party(self):
+        self.x = self.x + 1
+        print("So far", self.x)
+
+an = PartyAnimal()
+
+an.party()
+an.party()
+an.party()
